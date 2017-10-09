46050d6d645f26c8093c739c6021b1166e44766d
Unknown <vl979@nyu.edu>
2017-10-08 22:34:49 -0400

    Mostly Done WorldBuilding
    Need to iron out the rest of the placement of things in the world, but the majority of the modeling is done

---
+++ Assets/Scripts/RaycastGrounded.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastGrounded : MonoBehaviour {
+
+	public bool isGrounded = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//step one, declare ray object
+		Ray myRay = new Ray(transform.position, transform.right *-1f);
+
+		RaycastHit rayHit = new RaycastHit ();
+
+		//max distance for raycast
+		float maxDistance = 2f;
+
+		//optional but recommended, visualize raycast in scene debug
+		Debug.DrawRay(myRay.origin, myRay.direction*maxDistance, Color.red);
+
+		//step 4 actually shoot raycast
+		if (Physics.Raycast (myRay, maxDistance) && (rayHit.collider.gameObject.tag == "Wallet")) {
+			Debug.Log ("grounded");
+			isGrounded = true;
+		} else {
+			isGrounded = false;
+		}
+
+	}
+}
